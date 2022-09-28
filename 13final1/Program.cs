578c49c7298657543e75a2af382bc86d9f71464d
kamin0 <jm187@yandex.ru>
2022-09-28 23:43:15 +0300

    Добавьте файлы проекта.

---
+++ 13final1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.IO.MemoryMappedFiles;
+using System.Text;
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("List Added Time :");
+        CheckOptimization(CheckList);
+        Console.WriteLine();
+        Console.WriteLine("Linked List Added Time :");
+        CheckOptimization(CheckLinkedList);
+        Console.WriteLine();
+    }
+    public static void CheckList()
+    {
+        List<string> list = new List<string>();
+
+        using (var mappedFile1 = MemoryMappedFile.CreateFromFile("Text1.txt"))
+        {
+            using (Stream mmStream = mappedFile1.CreateViewStream())
+            {
+                using (StreamReader sr = new StreamReader(mmStream, ASCIIEncoding.ASCII))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var line = sr.ReadLine();
+                        var lineWords = line.Split(' ').ToList<string>();
+                        lineWords.ForEach(list.Add);
+                    }
+                }
+            }
+        }
+    }
+    public static void CheckLinkedList()
+    {
+        LinkedList<string> list = new LinkedList<string>();
+        using (var mappedFile1 = MemoryMappedFile.CreateFromFile("Text1.txt"))
+        {
+            using (Stream mmStream = mappedFile1.CreateViewStream())
+            {
+                using (StreamReader sr = new StreamReader(mmStream, ASCIIEncoding.ASCII))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var line = sr.ReadLine();
+                        var lineWords = line.Split(' ').ToList<string>();
+                        foreach (string word in lineWords)
+                        {
+                            list.AddLast(word);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public static void CheckOptimization(Action action)
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+        action?.Invoke(); // my method
+        stopWatch.Stop();
+        TimeSpan ts = stopWatch.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+        Console.WriteLine("RunTime " + elapsedTime);
+    }
+}
---
+++ 13final2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.IO.MemoryMappedFiles;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+List<string> list = ReadFile();
+
+Console.WriteLine("10 Наиболее часто встречающихся слов : \n");
+MostCommonWords(list).ToList().ForEach(Console.WriteLine);
+
+static List<string> ReadFile()
+{
+    List<string> list = new List<string>();
+    using (var mappedFile1 = MemoryMappedFile.CreateFromFile("Text1.txt"))
+    {
+        using (Stream mmStream = mappedFile1.CreateViewStream())
+        {
+            using (StreamReader sr = new StreamReader(mmStream, Encoding.UTF8))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+
+                    var noPunctuationText = new string(line.Where(c => !char.IsPunctuation(c)).ToArray());
+                    var lineWords = noPunctuationText.Split(' ').ToList<string>();
+                    lineWords.ForEach(list.Add);
+                }
+            }
+        }
+    }
+    return list;
+}
+
+static IEnumerable<string> MostCommonWords(List<string> list)
+{
+    Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+    foreach (string word in list)
+    {
+        if (!keyValuePairs.ContainsKey(word))
+        {
+            keyValuePairs.Add(word, 1);
+        }
+        else
+        {
+            keyValuePairs[word]++;
+        }
+    }
+    keyValuePairs = keyValuePairs.OrderBy(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+    var keyValuePairsReverse = keyValuePairs.Reverse();
+    var words = keyValuePairsReverse.Take(11).Select(x => x.Key);
+    return words;
+}
